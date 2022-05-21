84505a9c473eda24484295849497dee0de1c620f
Sheriff10 <ibrahimsheriff999@gmail.com>
2022-05-21 08:14:10 +0100

    upated mining to 10%

--- index.js
+++ index.js
@@ -308,7 +308,7 @@ app.post('/mine', async (req, res) => {
   // find deposit and filtering the active one
   const findDepo = await db.collection('deposits').find({email, status: 'confirmed'}).toArray();
   const fincActiveDepo = findDepo.filter(i => {
-    return i.active <= 60
+    return i.active <= 20
   });
   let total_investment = 0;
   for (i of fincActiveDepo) {
@@ -333,11 +333,11 @@ app.post('/mine', async (req, res) => {
     }
 
     if (days > 30) {
-      const commission = (total_investment * (4/100)) * (days - 70);
+      const commission = (total_investment * (10/100)) * (days - 70);
       db.collection('users').updateOne({email}, {$set: {balance: parseInt(user_cBal) + commission, login: update_cDate()}});
     }
     else{
-      const commission = (total_investment * (4/100)) * days;
+      const commission = (total_investment * (10/100)) * days;
       db.collection('users').updateOne({email}, {$set: {balance: user_cBal + commission, login: update_cDate()}});
     }
     const getUser2 = await db.collection('users').find({email}).toArray()
